413a12bb4e3b1867ee3e53851439492d778f9907
favasfaz <favaspgdi75@gmail.com>
2022-06-15 20:21:31 +0530

    after

--- connection/config.js
+++ connection/config.js
@@ -1,13 +1,10 @@
+
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://favasfaz:19thapril@cluster0.jybve.mongodb.net/shoppingcart?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-})
-
-mongoose.connection.on("error", err => {
-  console.log("err", err)
-})
-
-mongoose.connection.on("connected", (err, res) => {
-    console.log('mongoose is connected');
-})   
+mongoose.connect("mongodb+srv://favasfaz:19thapril@cluster0.jybve.mongodb.net/shoppingcart?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((_) => {
+   console.log("mongoose is connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
