f47543bddc9479efbe05a56b439ed41a65416a6b
favasfaz <favaspgdi75@gmail.com>
2022-09-01 11:38:37 +0530

    db config updated

--- connection/config.js
+++ connection/config.js
@@ -1,10 +1,13 @@
 
 const mongoose = require('mongoose');
+require('dotenv').config() 
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((_) => {
    console.log("mongoose is connected");
   })
   .catch((error) => {
+    //console.log(process.env.MONGO_URL,"process.env.MONGO_URL");
     console.log(error);
+
   });
